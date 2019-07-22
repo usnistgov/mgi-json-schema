12f49a99387af563db3831b7cf58436e10a4475c
Zachary Trautt <zachary.trautt@nist.gov>
2019-07-22 14:50:12 -0400

    add required to dereferencing tool

--- dereference.py
+++ dereference.py
@@ -19,14 +19,17 @@ for file_name_in in args['file']:
     json_data_out = jsonref.JsonRef.replace_refs(json_data_in, base_uri=base_uri)
     
     json_data_out["properties"] = dict()
+    req_list = list()
     
     if "allOf" in json_data_out.keys():
         for schema in json_data_out["allOf"]:
             for property_name,property_content in schema["properties"].items():
                 json_data_out["properties"][property_name] = property_content
-        
+            if "required" in schema:
+                for req_item in schema["required"]:
+                    req_list.append(req_item)
         json_data_out.pop("allOf")
-        
+        json_data_out["required"] = list(set(req_list))
     with open(file_name_out, mode='w+') as f:
         f.write(u'%s' % json.dumps(json_data_out,indent=4,separators=(',', ': ')))
     
