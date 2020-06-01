b9e7d606b87df785a98e986396560bd07fe48389
Zachary Trautt <zachary.trautt@nist.gov>
2020-06-01 13:17:54 -0400

    add lifecycle hooks

---
+++ dereferenced-schemas/Place.js
@@ -0,0 +1,18 @@
+var schema = require('/cordra/schemas/Place.schema.json');
+exports.beforeSchemaValidation = beforeSchemaValidation;
+
+function beforeSchemaValidation(obj, context) {
+    if (!context.useLegacyContentOnlyJavaScriptHooks) {
+        obj.content = beforeSchemaValidationLegacy(obj.content, context);
+        return obj;
+    } else {
+        return beforeSchemaValidationLegacy(obj, context);
+    }
+}
+
+function beforeSchemaValidationLegacy(obj, context) {
+    obj["@id"] = ""
+    obj["@context"] = schema["properties"]["@context"]["default"];
+    obj["@type"] = schema["properties"]["@type"]["default"];
+    return obj;
+}
